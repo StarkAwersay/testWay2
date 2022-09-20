7277a44a2cb89d495ef0f1ead9fc6f7fc905662f
StarkAwersay <4frankyyoung@gmail.com>
2022-09-20 14:06:55 +0400

    make DragNDrop test

---
+++ src/test/java/pages/DragAndDropPage.java
@@ -0,0 +1,45 @@
+package pages;
+
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import waits.Waiting;
+
+import static org.openqa.selenium.support.PageFactory.initElements;
+
+
+public class DragAndDropPage {
+    private WebDriver driver;
+    @FindBy(id = "draggable")
+    private WebElement draggableElement;
+
+    @FindBy(id = "droppable")
+    private WebElement droppableElement;
+
+    @FindBy(css = "[src*='default.html']")
+    private WebElement frame;
+
+    public DragAndDropPage(WebDriver driver) {
+        this.driver = driver;
+        initElements(driver, this);
+    }
+
+    @Step("Переключает на поле для работы с DragAndDrop")
+    public void switchFrame() {
+        driver.switchTo().frame(Waiting.waitingElementsDisplay(frame, driver));
+    }
+
+    @Step("Совершает DragAndDrop")
+    public void dragNDrop() {
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(Waiting.waitingElementsDisplay(draggableElement, driver), Waiting.waitingElementsDisplay(droppableElement, driver)).build().perform();
+    }
+
+    @Step("Получает текст droppable элемента")
+    public String getDroppableElementText() {
+        return droppableElement.getText();
+    }
+}
--- src/test/java/properties/Properties.java
+++ src/test/java/properties/Properties.java
@@ -6,4 +6,5 @@ public class Properties {
     public static final String PASSWORD_SQL_PAGE = "rec!vPL8aploX&";
     public static final String PROFILE_NAME_SQL_PAGE = "StarklAwersa";
     public static final String URL_HUB_GRID_SERVER = "http://26.115.101.38:4444";
+    public static final String DRAG_N_DROP_PAGE = "https://way2automation.com/way2auto_jquery/droppable.php#load_box";
 }
