ca09b67dbbff2578643c7fbfbe158a3be7d6706f
IlyaShpalochkin <4frankyyoung@gmail.com>
2023-07-04 18:30:49 +0400

    test api #6.1

---
+++ src/test/java/tests/api/CreatePost.java
@@ -0,0 +1,43 @@
+package tests.api;
+
+import constants.Constants;
+import helpers.JdbcTemplateHelper;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import io.restassured.RestAssured;
+import org.apache.hc.core5.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pojo.Posts;
+import rowMappers.PostRowMapper;
+import tables.Post;
+
+import static steps.ApiSteps.requestSpecification;
+
+@Epic("Api Тесты сайта test (wordpress)")
+public class CreatePost {
+    @BeforeEach
+    public void beforeApiTest() {
+        JdbcTemplateHelper.jdbcTemplate().update("delete from wp_posts");
+    }
+
+    @Feature("Тесты апи")
+    @Story("Тест добавление поста")
+    @Test
+    public void createPostTest() {
+        Posts parametersPost = new Posts("leader", "test", "publish");
+        RestAssured.given()
+                .spec(requestSpecification())
+                .body(parametersPost)
+                .post(Constants.END_POINT)
+                .then()
+                .statusCode(HttpStatus.SC_CREATED);
+        Post post = JdbcTemplateHelper.jdbcTemplate().queryForObject("SELECT *\n" +
+                "from wp_posts wp\n", new PostRowMapper());
+        Assert.assertEquals(parametersPost.getTitle(), post.getPostTitle());
+        Assert.assertEquals(parametersPost.getPassword(), post.getPastPassword());
+        Assert.assertEquals(parametersPost.getStatus(), post.getPostStatus());
+    }
+}
--- src/test/java/tests/api/UpdateAndDeletePost.java
+++ src/test/java/tests/api/UpdateAndDeletePost.java
@@ -7,9 +7,9 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import io.restassured.RestAssured;
 import org.apache.hc.core5.http.HttpStatus;
+import org.junit.jupiter.api.BeforeEach;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import pojo.Posts;
 import rowMappers.PostRowMapper;
@@ -19,11 +19,11 @@ import tables.Post;
 import static steps.ApiSteps.requestSpecification;
 
 @Epic("Api Тесты сайта test (wordpress)")
-public class ApiTests {
+public class UpdateAndDeletePost {
 
     private static Integer idCreatePost;
 
-    @BeforeTest
+    @BeforeEach
     public void beforeApiTest() {
         JdbcTemplateHelper.jdbcTemplate().update("delete from wp_posts");
     }
@@ -33,31 +33,11 @@ public class ApiTests {
         idCreatePost = ApiSteps.createPost();
     }
 
-    @Feature("Тесты апи")
-    @Story("Тест добавление поста")
-    @Test
-    public void createPostTest() {
-        Posts parametersPost = new Posts("leader", "test", "publish");
-        RestAssured.given()
-                .spec(requestSpecification())
-                .body(parametersPost)
-                .post(Constants.END_POINT)
-                .then()
-                .statusCode(HttpStatus.SC_CREATED);
-        Post post = JdbcTemplateHelper.jdbcTemplate().queryForObject("SELECT *\n" +
-                "from wp_posts wp\n", new PostRowMapper());
-        Assert.assertEquals(parametersPost.getTitle(), post.getPostTitle());
-        Assert.assertEquals(parametersPost.getPassword(), post.getPastPassword());
-        Assert.assertEquals(parametersPost.getStatus(), post.getPostStatus());
-    }
 
     @Feature("Тесты апи")
     @Story("Тест удаление поста")
     @Test()
     public void deletePostTest() {
-        Post post = JdbcTemplateHelper.jdbcTemplate().queryForObject("SELECT *\n" +
-                "from wp_posts wp where id like" + " " + idCreatePost, new PostRowMapper());
-        Assert.assertEquals("publish", post.getPostStatus());
         RestAssured.given()
                 .spec(requestSpecification())
                 .delete(Constants.END_POINT + idCreatePost)
@@ -73,8 +53,6 @@ public class ApiTests {
     @Test()
     public void updatePostTest() {
         Posts updatedPost = new Posts("leaders", "tests", "publish");
-        JdbcTemplateHelper.jdbcTemplate().queryForObject("SELECT *\n" +
-                "from wp_posts wp where id like" + " " + idCreatePost, new PostRowMapper());
         Integer oldId = ApiSteps.createPost();
         RestAssured.given()
                 .spec(requestSpecification())
